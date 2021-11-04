f732a1a463eaff502008e4443fa276e384f7f7fa
YifanZhangCWRU <32939404+YifanZhangCWRU@users.noreply.github.com>
2021-11-03 20:52:32 -0400

    Delete row3vectormakesparse.m

--- row3vectormakesparse.m
+++
@@ -1,7 +0,0 @@
-
-function output=row3vectormakesparse(btx,II,JJ,spinnumber)  
-% originally btx contains matrices with size [3 1 Ndict], contains no spin
-% info
-% sparse matrix is made by S{i(k),j(k)}=value(k)
-X= repmat (btx(:),[spinnumber,1]); 
-output     = sparse(II,JJ,X);
